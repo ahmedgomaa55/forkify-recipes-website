d89644b23a8b718f1c9fe54bb58cd8a5cf53e3a9
gomaa55 <ahmedgomaa1282000@gmail.com>
2024-01-03 19:21:15 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7ac61e57-a739-4b5b-9289-8c40bf5a6f1a';
+export const MODAL_CLOSE_SEC = 2.5;
